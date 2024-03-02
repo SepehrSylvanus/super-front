0c297e6d269543410210be825b1cc4a6dad0a70a
unknown <mr.forte.world@gmail.com>
2024-03-03 01:07:42 +0330

    setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=" fixed top-0 z-50 bg-b-8/90 backdrop-blur-sm border-n-6 lg:bg-n-8/90 lg:bckdrop-blur-sm">
+      Header
+    </div>
+  );
+};
+
+export default Header;
